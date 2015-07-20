8a4acaa8019c784f70c4ba087dabe4e6b4786e88
Phat Ky <kyp0717@gmail.com>
2015-07-20 14:49:12 -0400

    Create msgObj.js

---
+++ msgObj.js
@@ -0,0 +1,14 @@
+
+
+var connObj = { 
+  host: '127.0.0.1',
+  port: 4001 
+}
+
+// first submission is to tell TWS server which client version you are...
+var clientObj = {
+  client_version: 62,
+  client_initial: 0,
+  }
+
+var   
